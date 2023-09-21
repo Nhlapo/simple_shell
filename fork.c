c6a0c02d96c4ad0709747b3227c8af945499d62e
Winnie_Onyancha <wonyancha22@gmail.com>
2023-09-21 19:04:44 +0300

    Simple shell

--- fork.c
+++ fork.c
@@ -1,32 +1,111 @@
 #include "main.h"
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
+
+void print_prompt(void);
+void read_command(char *line);
+void execute_command(char *command);
+void handle_eof(void);
 
 /**
- * _fork_and_exe - fork and execute
- * @args: input
- * @argv: input
- * @cmd: input
- * Return: 0 or -1 even fork Failed
+ * print_prompt - Display the shell prompt.
  */
-
-int _fork_and_exe(char *args[], char *argv[], char *cmd)
+void print_prompt(void)
 {
-	pid_t pid = fork();
+	printf("$ ");
+}
+
+/**
+ * read_command - Read a command from the user.
+ * @line: A buffer to store the command line.
+ */
+void read_command(char *line)
+{
+	if (fgets(line, MAX_LINE_LENGTH, stdin) == NULL)
+	{
+		/* Handle end of file (Ctrl+D) */
+		handle_eof();
+	}
+
+	/* Remove the newline character from the end of the line */
+	line[strlen(line) - 1] = '\0';
+}
+
+/**
+ * execute_command - Execute a shell command.
+ * @command: The command to execute.
+ */
+void execute_command(char *command)
+{
+	pid_t child_pid;
 	int status;
 
-	if (pid < 0)
+	if (access(command, X_OK) == -1)
 	{
-		printf("Fork failed");
-		return (-1);
+		perror("access");
+		return;
 	}
-	else if (pid == 0)
+
+	/* Fork and execute the command */
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		args[0] = cmd;
-		args[1] = NULL;
-		execve(cmd, args, NULL);
-		printf("%s: No such file or directory\n", argv[0]);
-		exit(1);
+		perror("fork");
+		return;
 	}
-	else
-		waitpid(pid, &status, 0);
+	else if (child_pid == 0)
+	{
+		if (execlp(command, command, NULL) == -1)
+		{
+			perror("execlp");
+			exit(1);
+		}
+		exit(0);
+	}
+
+	/* Wait for the child process to finish */
+	if (waitpid(child_pid, &status, 0) == -1)
+	{
+		perror("waitpid");
+		return;
+	}
+
+	/* If child process exited with a non-zero status, print an error message */
+	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+	{
+		printf("Child exited with status %d\n", WEXITSTATUS(status));
+	}
+}
+
+/**
+ * handle_eof - Handle the "end of file" condition (Ctrl+D).
+ */
+void handle_eof(void)
+{
+	printf("\n");
+	exit(0);
+}
+
+/**
+ * main - The main entry point of the shell.
+ *
+ * Description: This function serves as the main entry point of the shell. It
+ * reads commands from the user, executes them, and displays the shell prompt.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	char line[MAX_LINE_LENGTH];
+
+	while (1)
+	{
+		print_prompt();
+		read_command(line);
+		execute_command(line);
+	}
+
 	return (0);
 }
